0160d39769c103b51fe8884f198a84aab05f0ad3
NatalyGVS <nataly.vasquez9718@gmail.com>
2019-06-06 17:41:55 -0500

    rest views

---
+++ application/views/dashboard.php
@@ -0,0 +1,119 @@
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <!-- <small>Panel de control</small> -->
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <?php if($is_admin == true): ?>
+
+        <div class="row">
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">  <!-- color celeste -->
+              <div class="inner">  <!-- como centrarlo  -->
+                <h3><?php echo $total_products ?></h3>
+
+                <p>
+                  <h2>Productos totales </h2>
+                
+              </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>   <!-- icono -->
+              </div>
+              <a href="<?php echo base_url('products/') ?>" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+
+
+          <!-- ./col -->
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+              <div class="inner">
+                <h3><?php echo $total_paid_orders ?></h3>
+                <p>
+                  <h2>Total de órdenes pagadas </h2>
+                
+              </p>
+              
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="<?php echo base_url('orders/') ?>" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+
+
+
+          <!-- ./col -->
+          <div class="col-lg-4 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <h3><?php echo $total_users; ?></h3>
+                 <p>
+                  <h2>Usuarios totales </h2>
+                 </p>
+              
+              </div>
+              <div class="icon">
+                <i class="ion ion-android-people"></i>
+              </div>
+              <a href="<?php echo base_url('users/') ?>" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+
+          <!-- ./col -->
+     <!--      <div class="col-lg-3 col-xs-6">
+            
+            <div class="small-box bg-red">
+              <div class="inner">
+                <h3><?php echo $total_stores ?></h3>
+                  <p>
+                  <h2>Total de tiendas </h2>
+                   </p>
+              
+              </div>
+              <div class="icon">
+                <i class="ion ion-android-home"></i>
+              </div>
+              <a href="<?php echo base_url('stores/') ?>" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div> -->
+
+
+
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+      <?php endif; ?>
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $("#dashboardMainMenu").addClass('active');
+    }); 
+  </script>
---
+++ application/views/users/create.php
@@ -0,0 +1,131 @@
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Gestionar Usuarios
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Usuarios</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-md-12 col-xs-12">
+          
+          <?php if($this->session->flashdata('success')): ?>
+            <div class="alert alert-success alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <?php echo $this->session->flashdata('success'); ?>
+            </div>
+          <?php elseif($this->session->flashdata('error')): ?>
+            <div class="alert alert-error alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <?php echo $this->session->flashdata('error'); ?>
+            </div>
+          <?php endif; ?>
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Agregar usuario</h3>
+            </div>
+            <form role="form" action="<?php base_url('users/create') ?>" method="post">
+              <div class="box-body">
+
+                <?php echo validation_errors(); ?>
+
+                <div class="form-group">
+                  <label for="groups">Los grupos</label>
+                  <select class="form-control" id="groups" name="groups">
+                    <option value="">Seleccionar grupos</option>
+                    <?php foreach ($group_data as $k => $v): ?>
+                      <option value="<?php echo $v['id'] ?>"><?php echo $v['group_name'] ?></option>
+                    <?php endforeach ?>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="username">Nombre de usuario</label>
+                  <input type="text" class="form-control" id="username" name="username" placeholder="Nombre de usuario" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Email" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="text" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="cpassword">Confirme password</label>
+                  <input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Confirme Password" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="fname">Nombre </label>
+                  <input type="text" class="form-control" id="fname" name="fname" placeholder="Nombre " autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="lname">Apellido</label>
+                  <input type="text" class="form-control" id="lname" name="lname" placeholder="Apellido" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="phone">Teléfono</label>
+                  <input type="text" class="form-control" id="phone" name="phone" placeholder="Teléfono" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                  <label for="gender">Género</label>
+                  <div class="radio">
+                    <label>
+                      <input type="radio" name="gender" id="male" value="1">
+                      Masculino
+                    </label>
+                    <label>
+                      <input type="radio" name="gender" id="female" value="2">
+                     Femenino
+                    </label>
+                  </div>
+                </div>
+
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                <a href="<?php echo base_url('users/') ?>" class="btn btn-warning">Atras</a>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- col-md-12 -->
+      </div>
+      <!-- /.row -->
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#groups").select2();
+
+    $("#mainUserNav").addClass('active');
+    $("#createUserNav").addClass('active');
+  
+  });
+</script>
