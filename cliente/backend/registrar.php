22bbda0c1d216f46d73454ae625212359c7d7058
iSeth1998 <duvanbradbrid@gmail.com>
2019-06-23 23:54:42 -0500

    tios

---
+++ cliente/backend/registrar.php
@@ -0,0 +1,12 @@
+<?php
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$gender = $_POST['gender'];
+
+
+	echo '<script type="text/javascript">alert("' . $firstname . '")</script>';
+
+
+?>
